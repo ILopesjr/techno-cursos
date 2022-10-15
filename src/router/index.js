2cf52dce0927d73fb5c7b4e71310c15d22276716
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2022-10-15 17:24:59 -0300

    component Header

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import Home from '../views/HomeView.vue';
+import Home from '../views/HomeView.vue';
+import Contato from '../views/ContatoView.vue';
+import Cursos from '../views/CursosView.vue';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/contato',
+    name: 'contato',
+    component: Contato,
+  },
+  {
+    path: '/cursos',
+    name: 'cursos',
+    component: Cursos,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
